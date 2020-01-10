70efed05377c3a55f91ad306d883c8aed63b1493
Mark Howison <mhowison@ripl.org>
2020-01-10 13:15:48 -0500

    reverted Figure 1; moving viral load data to a supplemental table

--- lib/Figure1.R
+++ lib/Figure1.R
@@ -1,16 +1,8 @@
 library(tidyverse)
-library(gridExtra)
 
 args <- commandArgs(trailingOnly=TRUE)
-
-samplefile <- args[1]
-distfiles  <- args[2:(length(args)-1)]
-outfile    <- args[length(args)]
-
-samples <- read_csv(samplefile) %>%
-           filter(Included == 1 & Analyte == "PL" & abs(VLToEnrollDays) < 90) %>%
-           mutate(Dataset=factor(MC)) %>%
-           select("Dataset", "VL")
+distfiles <- args[1:(length(args)-1)]
+outfile  <- args[length(args)]
 
 Gene <- c()
 Dataset <- c()
@@ -36,8 +28,7 @@ for (distfile in distfiles)
 
 data <- tibble(Gene=factor(Gene, levels=c("prrt", "int", "env", "wgs")),
                Dataset=factor(Dataset),
-               Diversity=Diversity) %>%
-        left_join(samples, by="Dataset")
+               Diversity=Diversity)
 
 # Print min/max ranges for each region
 print(data %>%
@@ -48,23 +39,11 @@ print(data %>%
 env <- filter(data, Gene=="env") %>% arrange(Diversity)
 data <- mutate(data, Dataset=factor(Dataset, levels=env$Dataset))
 
-p1 <- ggplot(data, aes(x=Dataset, y=VL)) +
-      geom_bar(stat="identity") +
-      labs(x="Patient", y="Viral Load") +
-      theme(axis.text.x=element_text(angle=90),
-            axis.text=element_text(size=6),
-            axis.title=element_text(size=9))
+g <- ggplot(data, aes(x=Dataset, y=Diversity, colour=Gene)) +
+     geom_point() +
+     labs(x="Patient", y="Intra-patient Genetic Diversity") +
+     scale_y_continuous(labels=scales::percent_format(accuracy=1)) +
+     theme(legend.position="right",
+           axis.text.x=element_text(angle = 90))
 
-p2 <- ggplot(data, aes(x=Dataset, y=Diversity, colour=Gene)) +
-      geom_point() +
-      labs(x="Patient", y="Within-host Genetic Diversity") +
-      scale_y_continuous(labels=scales::percent_format(accuracy=1)) +
-      theme(legend.position="bottom",
-            axis.text.x=element_text(angle=90),
-            axis.text=element_text(size=6),
-            axis.title=element_text(size=9),
-            legend.text=element_text(size=11))
-
-pdf(outfile, width=10, height=5)
-grid.arrange(p1, p2, ncol=1, heights=c(1,2))
-dev.off()
+ggsave(outfile, g, width=12, height=3.5, units="in")
